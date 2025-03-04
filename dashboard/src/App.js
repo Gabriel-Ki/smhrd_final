e159e10a97a53180a16e18e4c37e9ed172c43c99
ch6561 <zeus6561@naver.com>
2025-03-04 11:38:19 +0900

    첫번째 커밋

---
+++ dashboard/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import MainDash from './Main/header/Main_header'
+import Main_card from './Main/card/Main_card';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <MainDash/> */}
+      <Main_card/>
+    </div>
+  );
+}
+
+export default App;
