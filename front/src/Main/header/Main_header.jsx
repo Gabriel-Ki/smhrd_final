f010fb318afba9ddd0cc919838d6889731ec046c
ch6561 <zeus6561@naver.com>
2025-03-04 17:06:33 +0900

    주석 포함

--- front/src/Main/header/Main_header.jsx
+++ front/src/Main/header/Main_header.jsx
@@ -5,13 +5,13 @@ const Main_dash = () => {
     <div className='head-container'>
         <h3 className='header-title'>팀장선</h3>
         <div className='robot-status'>
-          <span className='allrobot'>전체 로봇 8</span> 
+          <span className='allrobot'>전체 로봇 8</span> {/* 여기서 숫자api로 값 가져와서 넣은 곳 */}
           <div className='fliter'>
-            <span>수행 중 4</span>
-            <span>대기 중 1</span>
-            <span>충전 중 1</span>
-            <span className='error'>에러 1</span>
-            <span className='emergency-stop'>비상 정지 1</span>
+            <span>수행 중 4</span>{/*여기서 숫자 api로 값 가져와서 넣은 곳 */}
+            <span>대기 중 1</span>{/* 여기서 숫자api로 값 가져와서 넣은 곳 */}
+            <span>충전 중 1</span>{/* 여기서 숫자api로 값 가져와서 넣은 곳 */}
+            <span className='error'>에러 1</span>{/* 여기서 숫자api로 값 가져와서 넣은 곳 */}
+            <span className='emergency-stop'>비상 정지 1</span>{/* 여기서 숫자api로 값 가져와서 넣은 곳 */} 
           </div>
           
         </div>
