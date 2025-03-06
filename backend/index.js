c7b622ea3744876b65434301b43e1e53c970b198
JHW <ruby07025@gmail.com>
2025-03-06 09:59:25 +0900

    테이블 불러오기

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const pool = require('./db');
 
 async function getUsers() {
   try {
-    const [rows] = await pool.query('SELECT * FROM users');
+    const [rows] = await pool.query('SELECT * FROM js_user');
     console.log('사용자 목록:', rows);
   } catch (error) {
     console.error('쿼리 실행 실패:', error);
