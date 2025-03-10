3ed6acde31f007ac4000d2c5e81f52d2125c6918
JHW <ruby07025@gmail.com>
2025-03-10 16:58:22 +0900

    db

--- backend/router/mainrouter.js
+++ backend/router/mainrouter.js
@@ -2,17 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 // 샘플 데이터 (API가 제공하는 로봇 상태 데이터)
-const robots = [
-    { id: 1, name: '프로그램명 -1', status: '작업 중' },
-    { id: 2, name: '프로그램명 -2', status: '대기 중' },
-    { id: 3, name: '프로그램명 -3', status: '작업 중' },
-    { id: 4, name: '프로그램명 -4', status: '작업 중' },
-    { id: 5, name: '프로그램명 -5', status: '작업 중' },
-    { id: 6, name: '프로그램명 -6', status: '에러' }
-];
+// const robots = [
+//     { id: 1, name: '프로그램명 -1', status: '작업 중' },
+//     { id: 2, name: '프로그램명 -2', status: '대기 중' },
+//     { id: 3, name: '프로그램명 -3', status: '작업 중' },
+//     { id: 4, name: '프로그램명 -4', status: '작업 중' },
+//     { id: 5, name: '프로그램명 -5', status: '작업 중' },
+//     { id: 6, name: '프로그램명 -6', status: '에러' }
+// ];
 
 // 모든 로봇 데이터를 반환하는 API
-router.get('/robots', (req, res) => {
+router.get('/robots',async (req, res) => {
+    try {
+      const [rows] = await pool.query('SELECT * FROM js_users');
+      res.json(rows);
+    } catch (error) {
+      console.error('쿼리 실행 실패:', error);
+      res.status(500).json({ message: '서버 오류' });
+    }
     res.json(robots);
 });
 
