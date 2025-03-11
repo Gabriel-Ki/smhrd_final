ac198b15d72e71dea2f7d7b82586a31f5fbcf021
ch6561 <zeus6561@naver.com>
2025-03-11 15:21:43 +0900

    boom

--- backend/router/mainrouter.js
+++ backend/router/mainrouter.js
@@ -2,18 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pool=require('../db')
 
-// 샘플 데이터 (API가 제공하는 로봇 상태 데이터)
-// const robots = [
-//     { id: 1, name: '프로그램명 -1', status: '작업 중' },
-//     { id: 2, name: '프로그램명 -2', status: '대기 중' },
-//     { id: 3, name: '프로그램명 -3', status: '작업 중' },
-//     { id: 4, name: '프로그램명 -4', status: '작업 중' },
-//     { id: 5, name: '프로그램명 -5', status: '작업 중' },
-//     { id: 6, name: '프로그램명 -6', status: '에러' }
-// ];
-
 router.get('/dashboard', async(req,res)=>{
-    const sql='select * from mainpage'
+    const sql="select * from robot_status "
     try{
         const [results]= await pool.query(sql)
         res.status(200).json(results)
@@ -21,6 +11,17 @@ router.get('/dashboard', async(req,res)=>{
         console.error('데이터 베이스 조회 오류:', err);
         res.status(500).json({error: '연결 실패', details: err.message})
 
+    }
+})
+
+router.get('/log', async(req,res)=>{
+    const sql="SELECT robot_id, status, destination, log_m,  DATE_FORMAT(log_t, '%H:%i') AS log_t FROM (SELECT robot_id, status, destination, log_m, log_t, ROW_NUMBER() OVER (PARTITION BY robot_id ORDER BY log_t DESC) AS rn FROM robot_logs) AS ranked_logs WHERE rn <= 3"
+    try{
+        const [results]=await pool.query(sql)
+        res.status(200).json(results)
+    }catch(err){
+        console.error('db 조ㅗ회 오류', err);
+        res.status(500).json({error:'연결 실패', details:err.message})
     }
 })
 
