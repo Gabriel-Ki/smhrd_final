be77f784e906b10f12506bcde33331a9bffb117e
ch6561 <zeus6561@naver.com>
2025-03-18 11:03:28 +0900

    경로 기능 구현

--- front/src/Mappage/DeliveryPage.jsx
+++ front/src/Mappage/DeliveryPage.jsx
@@ -5,68 +5,46 @@ import '../Mappage/Deliverypage.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import '../header.css'
-// import '../layout/Layout.css'
 
 const DeliveryPage = () => {
-
   const navigate = useNavigate();
-  const [robots,setRobots]=useState([]) // 로봇 정보 담고
-  const [selectRobot,setSelectRobot]=useState(null); // 지도에서 클릭한 로봇의 id
-  // const [clickRobot,setClickRobot]=useState(); 
+  const [robots, setRobots] = useState([]); // 로봇 정보 담기
+  const [selectRobot, setSelectRobot] = useState(null); // 지도에서 클릭한 로봇의 id
 
-  // const [sideRobots,setSideRobots]=useState([]); //sidebar에 넘길 값들만 뽑은
-  // const [mapRobots,setMapRobots]=useState([]); // map에 넘길 값들만 뽑은 
-
-  useEffect(()=>{
-    const axiosRobot=async ()=>{
-      try{
-        const response= await axios.get('http://localhost:5000/robot'); // 경로 수정하고 로봇 6개의 정보를 여기로 출력
+  // 로봇 데이터 가져오기
+  useEffect(() => {
+    const axiosRobot = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/robot');
         setRobots(response.data);
-        
-      } catch(error){
-        console.error('로봇 정보를 가져오는 중 오류 :', error);
-      } 
+      } catch (error) {
+        console.error('로봇 정보를 가져오는 중 오류:', error);
+      }
     }
     axiosRobot();
     
-    const intervaild=setInterval(axiosRobot,180000); // 10초 마다 업데이트 하겠다
+    // 3분(180000ms)마다 로봇 정보 업데이트
+    const intervalId = setInterval(axiosRobot, 180000);
+    
+    return () => clearInterval(intervalId);
+  }, []);
 
-    return ()=>clearInterval(intervaild);
-
-    },[]);
-
-    // console.log(robots);
-
-
-    const onMarkerClick=(robot)=>{
-
-      if (selectRobot===robot){
-        setSelectRobot(null)
-      }else{
-        setSelectRobot(robot);
-      }
-      // console.log(robot, '마커 클릭');
-      
-      
+  // 마커 클릭 핸들러
+  const onMarkerClick = (robot) => {
+    if (selectRobot === robot) {
+      setSelectRobot(null);
+    } else {
+      setSelectRobot(robot);
     }
+  };
 
-    const NaviRobot=robots.find(robot=> robot.robots_idx === selectRobot)
-    // console.log(NaviRobot)
-
-  
-
-    // console.log('deliverypage',onMarkerClick);
-
-    
-    // console.log(clickRobot);
-    
-    // console.log(selectRobot);
-
+  // 선택된 로봇 객체 가져오기
+  const selectedRobotObject = robots.find(robot => robot.robots_idx === selectRobot);
 
   return (
     <div className='delivery-page'>
       <div className='common-header'>
-      <span className='header-title' >Delivus</span>
+        <span className='header-title'>Delivus</span>
         <div className='common-header-buttons'>
           <button onClick={() => navigate('/maindash')}>대시보드</button>
           <button onClick={() => navigate('/')}>주문 내역</button>
@@ -74,19 +52,19 @@ const DeliveryPage = () => {
       </div>
       
       <div className='delivery-container'>
-        {/* 지도와 사이드바 */}
         <div className='delivery-box'>
-          {/* 지도 */}
           <div className='delivery-map'>
-            <KakaoMap robots={robots} onMarkerClick={onMarkerClick} clickRobot={NaviRobot}/>
+            <KakaoMap 
+              robots={robots} 
+              onMarkerClick={onMarkerClick} 
+              clickRobot={selectedRobotObject} 
+            />
           </div>
-          {/* 사이드바 */}
           <div className='delivery-sidebar'>
-            <MapSidebar selectRobot={selectRobot} robots={robots}/>
+            <MapSidebar selectRobot={selectRobot} robots={robots} />
           </div>
         </div>
       </div>
-
     </div>
   )
 }
